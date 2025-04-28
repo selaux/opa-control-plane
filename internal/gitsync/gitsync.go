994f8aeff0f4b145c0657ef92a719b9eec1e639a
Teemu Koponen <koponen@styra.com>
2025-04-28 11:12:29 -0700

    gitsync: A placeholder.

---
+++ internal/gitsync/gitsync.go
@@ -0,0 +1,3 @@
+// gitsync package implements Git synchronization. It maintains a local filesystem copy for each configured
+// git reference, periodically fetching updates from the remote repositories.
+package gitsync
