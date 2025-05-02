db6b9f55ae7ea8cd85c31e4132696fbd827814c9
Teemu Koponen <koponen@styra.com>
2025-05-02 09:50:04 -0700

    internal/service: Skeleton for HTTP pull data source.

---
+++ internal/httpsync/httpsync.go
@@ -0,0 +1,36 @@
+package httpsync
+
+import (
+	"io"
+	"net/http"
+	"os"
+)
+
+// HttpDataSynchronizer is a struct that implements the Synchronizer interface for downloading JSON from HTTP endpoints.
+type HttpDataSynchronizer struct {
+	path string // The path where the data will be saved
+	url  string
+	// TODO: Add more fields if needed, such as headers, authentication, etc.
+}
+
+func NewHttpDataSynchronizer(path string, url string) HttpDataSynchronizer {
+	return HttpDataSynchronizer{path: path, url: url}
+}
+
+func (s HttpDataSynchronizer) Execute() error {
+	f, err := os.Create(s.path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	resp, err := http.Get(s.url)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	return err
+}
