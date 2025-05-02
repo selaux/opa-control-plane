259fbc134432fdea359d0630863f139cc7ef12f6
Teemu Koponen <koponen@styra.com>
2025-05-02 13:16:29 -0700

    internal/httpsync: Missing test file.

---
+++ internal/httpsync/httpsync_test.go
@@ -0,0 +1,40 @@
+package httpsync
+
+import (
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestHTTPDataSynchronizer(t *testing.T) {
+	contents := `{"key": "value"}`
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		_, err := w.Write([]byte(contents))
+		if err != nil {
+			http.Error(w, "failed to write response", http.StatusInternalServerError)
+		}
+	}))
+	defer ts.Close()
+
+	file := path.Join(t.TempDir(), "foo/test.json")
+	synchronizer := New(file, ts.URL)
+	err := synchronizer.Execute()
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	data, err := os.ReadFile(file)
+	if err != nil {
+		t.Fatalf("expected no error while reading file, got: %v", err)
+	}
+
+	if !bytes.Equal(data, []byte(contents)) {
+		t.Fatal("downloaded data does not match expected contents")
+	}
+}
