23e47f0985728a6a7c1571d9ba6510205e74737b
Teemu Koponen <koponen@styra.com>
2025-05-08 12:22:27 -0700

    internal/database: Refactor tests to prep for more tests.

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -6,6 +6,7 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/tsandall/lighthouse/internal/database"
 	"github.com/tsandall/lighthouse/internal/service"
 )
 
@@ -20,45 +21,78 @@ func TestDatabaseSystemsData(t *testing.T) {
 
 	defer db.CloseDB()
 
-	data, found, err := db.SystemsDataGet(ctx, "system1", "foo")
-	if data != nil || found || err != nil {
-		t.Fatalf("expected no data to be found, got %v (%v), err: %v", data, found, err)
+	data1 := map[string]interface{}{
+		"key": "value1",
 	}
-
-	// Test that we can put data
-
-	data = map[string]interface{}{
-		"key": "value",
-	}
-	if err := db.SystemsDataPut(ctx, "system1", "foo", data); err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
-
-	data2, found, err := db.SystemsDataGet(ctx, "system1", "foo")
-	if !reflect.DeepEqual(data, data2) || !found || err != nil {
-		t.Fatalf("expected data to be found, got %v (%v), err: %v", data, found, err)
-	}
-
-	// Test that we can update the data
-
-	data = map[string]interface{}{
+	data2 := map[string]interface{}{
 		"key": "value2",
 	}
-	if err := db.SystemsDataPut(ctx, "system1", "foo", data); err != nil {
-		t.Fatalf("expected no error, got %v", err)
-	}
-	data2, found, err = db.SystemsDataGet(ctx, "system1", "foo")
-	if !reflect.DeepEqual(data2, data) || !found || err != nil {
-		t.Fatalf("expected data to be found, got %v (%v), err: %v", data2, found, err)
+
+	tests := []*testCase{
+		newTestCase("get non-existing data").Get("system1", "foo", nil),
+		newTestCase("put data").Put("system1", "foo", data1).Get("system1", "foo", data1),
+		newTestCase("update data").Put("system1", "foo", data2).Get("system1", "foo", data2),
+		newTestCase("delete").Delete("system1", "foo").Get("system1", "foo", nil),
 	}
 
-	// Test that we can delete the data
+	for _, test := range tests {
+		t.Run(test.note, func(t *testing.T) {
+			for _, op := range test.operations {
+				op(ctx, t, db)
+			}
+		})
+	}
+}
 
-	if err := db.SystemsDataDelete(ctx, "system1", "foo"); err != nil {
-		t.Fatalf("expected no error, got %v", err)
+type testCase struct {
+	note       string
+	operations []func(ctx context.Context, t *testing.T, db *database.Database)
+}
+
+func newTestCase(note string) *testCase {
+	return &testCase{
+		note:       note,
+		operations: nil,
 	}
-	data2, found, err = db.SystemsDataGet(ctx, "system1", "foo")
-	if data2 != nil || found || err != nil {
-		t.Fatalf("expected no data to be found, got %v (%v), err: %v", data2, found, err)
-	}
+}
+
+func (tc *testCase) Get(systemID, dataID string, expected interface{}) *testCase {
+	tc.operations = append(tc.operations, func(ctx context.Context, t *testing.T, db *database.Database) {
+		data, found, err := db.SystemsDataGet(ctx, systemID, dataID)
+		if err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+
+		switch {
+		case found && expected == nil:
+			t.Fatal("expected no data to be found")
+		case !found && expected != nil:
+			t.Fatal("expected data to be found")
+		case !found && expected == nil:
+			// OK
+		case found && expected != nil:
+			if !reflect.DeepEqual(expected, data) {
+				t.Fatalf("expected data not found, got %v", data)
+			}
+		}
+	})
+	return tc
+}
+
+func (tc *testCase) Put(systemID, dataID string, data interface{}) *testCase {
+	tc.operations = append(tc.operations, func(ctx context.Context, t *testing.T, db *database.Database) {
+		if err := db.SystemsDataPut(ctx, systemID, dataID, data); err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+	})
+	return tc
+}
+
+func (tc *testCase) Delete(systemID, dataID string) *testCase {
+	tc.operations = append(tc.operations, func(ctx context.Context, t *testing.T, db *database.Database) {
+		if err := db.SystemsDataDelete(ctx, systemID, dataID); err != nil {
+			t.Fatalf("expected no error, got %v", err)
+		}
+	})
+	return tc
 }
