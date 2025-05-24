d292a1b1cde55b3a4dc0178ca06b79417631e72f
Torin Sandall <torin@styra.com>
2025-05-24 06:53:36 -0700

    cmd: add support for config directories
    Also, fix unmarshal call that was panicing because value was not passed
    by reference.

--- cmd/run.go
+++ cmd/run.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/tsandall/lighthouse/internal/server"
@@ -74,14 +76,28 @@ func init() {
 
 func getMergedConfig(configFiles []string) ([]byte, error) {
 
-	var docs []map[string]interface{}
+	var paths []string
 	for _, f := range configFiles {
+		filepath.Walk(f, func(path string, fi fs.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+			if fi.IsDir() {
+				return nil
+			}
+			paths = append(paths, path)
+			return nil
+		})
+	}
+
+	var docs []map[string]interface{}
+	for _, f := range paths {
 		bs, err := os.ReadFile(f)
 		if err != nil {
 			return nil, fmt.Errorf("failed to read configuration file %v: %v", f, err)
 		}
 		var x map[string]interface{}
-		if err := yaml.Unmarshal(bs, x); err != nil {
+		if err := yaml.Unmarshal(bs, &x); err != nil {
 			return nil, fmt.Errorf("failed to unmarshal configuration file %v: %v", f, err)
 		}
 		docs = append(docs, x)
