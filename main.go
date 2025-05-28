a77fcb3b9c7d17e75c52edf24ef5680c50069290
Torin Sandall <torin@styra.com>
2025-05-28 09:17:30 -0700

    cmd: fix command registration

--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"os"
 
 	"github.com/tsandall/lighthouse/cmd"
+	_ "github.com/tsandall/lighthouse/cmd/backtest"
+	_ "github.com/tsandall/lighthouse/cmd/compare"
+	_ "github.com/tsandall/lighthouse/cmd/migrate"
+	_ "github.com/tsandall/lighthouse/cmd/run"
+	_ "github.com/tsandall/lighthouse/cmd/version"
 )
 
 func main() {
