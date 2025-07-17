bf4441ded7a13de833dfbf77783dc6d7b47afce2
Teemu Koponen <koponen@styra.com>
2025-07-17 14:25:05 -0700

    internal/authz: Improved authz test.

--- internal/authz/authz_test.go
+++ internal/authz/authz_test.go
@@ -2,15 +2,78 @@ package authz
 
 import (
 	"context"
-	"fmt"
+	"database/sql"
 	"testing"
+
+	_ "modernc.org/sqlite"
 )
 
-// TODO(tsandall): add integration test
-func testPartial(t *testing.T) {
-	result, err := Partial(context.Background(), Access{Principal: "bob", Resource: "sources", Permission: "sources.view"}, map[string]ColumnRef{"input.id": {Table: "sources", Column: "id"}})
+func TestPartial(t *testing.T) {
+	db, err := sql.Open("sqlite", "file::memory:?cache=shared")
 	if err != nil {
 		t.Fatal(err)
 	}
-	fmt.Println(result.SQL())
+
+	defer db.Close()
+
+	db.Exec("CREATE TABLE sources (name TEXT)")
+	db.Exec("CREATE TABLE principals (id TEXT, role TEXT)")
+	db.Exec("CREATE TABLE resource_permissions (name TEXT, resource TEXT, principal_id TEXT, role TEXT, permission TEXT)")
+
+	db.Exec("INSERT INTO sources (name) VALUES ('source')")
+	db.Exec("INSERT INTO principals (id, role) VALUES ('alice', 'administrator')")
+	db.Exec("INSERT INTO principals (id, role) VALUES ('bob', 'viewer')")
+	db.Exec("INSERT INTO resource_permissions (name, resource, principal_id, role, permission) VALUES ('source', 'sources', 'bob', 'viewer', 'sources.viewer')")
+
+	testCases := []struct {
+		name                string
+		access              Access
+		extraColumnMappings map[string]ColumnRef
+		allow               bool
+	}{
+		{
+			name:   "allow access",
+			access: Access{Principal: "alice", Resource: "sources", Permission: "sources.create"},
+			allow:  true, // alice admin has full access
+		},
+		{
+			name:   "deny access",
+			access: Access{Principal: "bob", Resource: "sources", Permission: "sources.create"},
+			allow:  false, // bob viewer not allowed to create
+		},
+		{
+			name:   "allow with extra columns",
+			access: Access{Principal: "bob", Resource: "sources", Permission: "sources.viewer"},
+			extraColumnMappings: map[string]ColumnRef{
+				"input.name": {Table: "sources", Column: "name"},
+			},
+			allow: true, // bob can view resource he has permission for
+		},
+		{
+			name:   "deny with extra columns",
+			access: Access{Principal: "bob", Resource: "sources", Permission: "sources.create"},
+			extraColumnMappings: map[string]ColumnRef{
+				"input.name": {Table: "sources", Column: "name"},
+			},
+			allow: false, // bob viewer not allowed to create, only view the resource
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := Partial(context.Background(), tc.access, tc.extraColumnMappings)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			rows, err := db.Query("SELECT * FROM sources WHERE " + result.SQL())
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if rows.Next() != tc.allow {
+				t.Fatalf("expected allow %v, got %v", tc.allow, !tc.allow)
+			}
+		})
+	}
 }
